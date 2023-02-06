f1e5ff8d6b36bd72dab51e94121bff8123cf2591
reflake <reflakeykt@gmail.com>
2023-02-06 16:52:27 +0600

    add transition to menu when game is over after a while (transition function is a stub and main menu is yet to be implemented!)

--- Assets/Code/Script/GameManager.cs
+++ Assets/Code/Script/GameManager.cs
@@ -1,11 +1,22 @@
-﻿using UnityEngine;
+﻿using System;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
 	public event GameOverDelegate OnGameOver;
 	
-	public void GameOver()
+	public async void GameOver()
 	{
 		OnGameOver.Invoke();
+
+		await UniTask.Delay(TimeSpan.FromSeconds(5f), DelayType.Realtime, PlayerLoopTiming.Update);
+		
+		GoToMainMenu();
+	}
+
+	private void GoToMainMenu()
+	{
+		throw new NotImplementedException("Cannot transit to main menu: main menu is yet to be implemented!");
 	}
 }
