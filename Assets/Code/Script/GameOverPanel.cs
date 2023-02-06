e578b402c4ed31be0e6042fc3387aa9a861eb35e
reflake <reflakeykt@gmail.com>
2023-02-06 16:46:48 +0600

    basic game over panel appearance logic

---
+++ Assets/Code/Script/GameOverPanel.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+using Zenject;
+
+public class GameOverPanel : MonoBehaviour
+{
+	[SerializeField] private CanvasGroup _canvasGroup = null;
+
+	[Inject] private readonly GameManager _gameManager = null;
+
+	[Inject]
+	public void Construct()
+	{
+		_gameManager.OnGameOver += ShowPanel;
+	}
+	
+	public void ShowPanel()
+	{
+		_canvasGroup.alpha = 1f;
+	}
+}
