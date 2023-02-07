ddedc015b5f2ad5e941283e240fcb1f4cf939499
reflake <reflakeykt@gmail.com>
2023-02-07 16:09:30 +0600

    replace async void with UniTaskVoid

--- Assets/Code/Script/GameManager.cs
+++ Assets/Code/Script/GameManager.cs
@@ -6,7 +6,7 @@ public class GameManager : MonoBehaviour
 {
 	public event GameOverDelegate OnGameOver;
 	
-	public async void GameOver()
+	public async UniTaskVoid GameOver()
 	{
 		OnGameOver.Invoke();
 
