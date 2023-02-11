eb4448bbf1551e31d7397d5ae05d4405f64482bc
reflake <reflakeykt@gmail.com>
2023-02-11 20:30:40 +0600

    battlefield class

--- Assets/Code/Script/BattleField.cs
+++ Assets/Code/Script/BattleField.cs
@@ -2,5 +2,20 @@
 
 public class BattleField : MonoBehaviour
 {
-	[SerializeField] private Bounds bounds;
+	[SerializeField] private Grid grid;
+	[SerializeField] private BoundsInt bounds;
+
+	private void OnDrawGizmosSelected()
+	{
+		if (grid)
+		{
+			Gizmos.color = Color.yellow;
+
+			var min = grid.CellToWorld(bounds.min);
+			var max = grid.CellToWorld(bounds.max);
+			var size = max - min;
+			
+			Gizmos.DrawWireCube(min + size / 2, size);
+		}
+	}
 }
