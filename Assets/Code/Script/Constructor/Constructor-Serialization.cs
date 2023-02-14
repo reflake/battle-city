79ea366ced67143cb924d514a4d22a7bb6dd6f0d
reflake <reflakeykt@gmail.com>
2023-02-14 13:40:17 +0600

    refactor constructor - move serialization method outside

---
+++ Assets/Code/Script/Constructor/Constructor-Serialization.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace LevelDesigner
+{
+	public partial class Constructor
+	{
+		[SerializeField] TileList tileData;
+		
+		public LevelData GetSerializedData()
+		{
+			// Find and write individual tile names
+			var usedTilesCount = tilemap.GetUsedTilesCount();
+			var usedTiles = new TileBase[usedTilesCount];
+
+			tilemap.GetUsedTilesNonAlloc(usedTiles);
+
+			var int2name = new Dictionary<int, string>();
+			var name2int = new Dictionary<string, int>();
+
+			int2name.Add(0, "null");
+
+			int index = 1;
+
+			foreach (var tile in usedTiles)
+			{
+				name2int[tile.name] = index;
+				int2name[index++] = tile.name;
+			}
+
+			// Prepare tilemaps for serialization
+			tilemap.CompressBounds();
+
+			// Find all tiles on layer and serialize as index of name
+			var bounds = tilemap.cellBounds;
+			var tilesArray = tilemap.GetTilesBlock(bounds);
+
+			var tilesData = tilesArray
+				.Select(tile => tile != null ? name2int[tile.name] : 0)
+				.ToArray();
+
+			return new LevelData
+			{
+				tileMapIds = int2name.Keys.ToArray(),
+				tileMapNames = int2name.Values.ToArray(),
+				boundsX = bounds.x,
+				boundsY = bounds.y,
+				boundsZ = bounds.z,
+				boundsW = bounds.size.x,
+				boundsH = bounds.size.y,
+				boundsD = bounds.size.z,
+				tilesData = tilesData,
+			};
+		}
+
+		public void LoadLevel(LevelData levelData)
+		{
+			// Unload current map
+			tilemap.ClearAllTiles();
+
+			// List all names of used tiles
+			var tileByName = new Dictionary<int, TileBase>();
+
+			for (int i = 0; i < levelData.tileMapNames.Length; i++)
+			{
+				var tileId = levelData.tileMapIds[i];
+				var tileName = levelData.tileMapNames[i];
+
+				if (tileName == "null")
+				{
+					tileByName.Add(tileId, null);
+				}
+				else
+				{
+					var tileRef = tileData.tiles.Single(x => x.name == tileName);
+
+					tileByName.Add(tileId, tileRef);
+				}
+			}
+
+			// Load new map
+			var bounds = new BoundsInt(
+				levelData.boundsX, levelData.boundsY, levelData.boundsZ,
+				levelData.boundsW, levelData.boundsH, levelData.boundsD);
+
+			var tiles = levelData.tilesData
+				.Select(tileId => tileByName[tileId])
+				.ToArray();
+
+			tilemap.SetTilesBlock(bounds, tiles);
+		}
+	}
+}
--- Assets/Code/Script/Constructor/Constructor.cs
+++ Assets/Code/Script/Constructor/Constructor.cs
@@ -13,7 +13,7 @@ using Zenject;
 
 namespace LevelDesigner
 {
-	public class Constructor : MonoBehaviour
+	public partial class Constructor : MonoBehaviour
 	{
 		[SerializeField] SpriteRenderer cursorSprite;
 		
@@ -21,10 +21,6 @@ namespace LevelDesigner
 		
 		[SerializeField] Tilemap tilemap;
 		[SerializeField] GameObject blocksPrefab;
-		
-		[Space]
-		
-		[SerializeField] TileList tileData;
 
 		[Inject] readonly BattleField _battleField;
 		
@@ -173,89 +169,6 @@ namespace LevelDesigner
 		{
 			Gizmos.color = Color.green;
 			Gizmos.DrawCube(transform.position, Vector3.one);
-		}
-
-		public LevelData GetSerializedData()
-		{
-			// Find and write individual tile names
-			var usedTilesCount = tilemap.GetUsedTilesCount();
-			var usedTiles = new TileBase[usedTilesCount];
-
-			tilemap.GetUsedTilesNonAlloc(usedTiles);
-			
-			var int2name = new Dictionary<int, string>();
-			var name2int = new Dictionary<string, int>();
-
-			int2name.Add(0, "null");
-
-			int index = 1;
-				
-			foreach (var tile in usedTiles)
-			{
-				name2int[tile.name] = index;
-				int2name[index++] = tile.name;
-			}
-			
-			// Prepare tilemaps for serialization
-			tilemap.CompressBounds();
-
-			// Find all tiles on layer and serialize as index of name
-			var bounds = tilemap.cellBounds;
-			var tilesArray = tilemap.GetTilesBlock(bounds);
-			
-			var tilesData = tilesArray
-				.Select(tile => tile != null ? name2int[tile.name] : 0)
-				.ToArray();
-
-			return new LevelData
-			{
-				tileMapIds = int2name.Keys.ToArray(),
-				tileMapNames = int2name.Values.ToArray(),
-				boundsX = bounds.x,
-				boundsY = bounds.y,
-				boundsZ = bounds.z,
-				boundsW = bounds.size.x,
-				boundsH = bounds.size.y,
-				boundsD = bounds.size.z,
-				tilesData = tilesData,
-			};
-		}
-
-		public void LoadLevel(LevelData levelData)
-		{
-			// Unload current map
-			tilemap.ClearAllTiles();
-			
-			// List all names of used tiles
-			var tileByName = new Dictionary<int, TileBase>();
-			
-			for (int i = 0; i < levelData.tileMapNames.Length; i++)
-			{
-				var tileId = levelData.tileMapIds[i];
-				var tileName = levelData.tileMapNames[i];
-
-				if (tileName == "null")
-				{
-					tileByName.Add(tileId, null);
-				}
-				else
-				{
-					var tileRef = tileData.tiles.Single(x => x.name == tileName);
-					
-					tileByName.Add(tileId, tileRef);
-				}
-			}
-			
-			// Load new map
-			var bounds = new BoundsInt(
-				levelData.boundsX, levelData.boundsY, levelData.boundsZ,
-				levelData.boundsW, levelData.boundsH, levelData.boundsD);
-
-			var tiles = levelData.tilesData
-											.Select(tileId => tileByName[tileId])
-											.ToArray();
-			
-			tilemap.SetTilesBlock(bounds, tiles);
 		}
 	}
 }
