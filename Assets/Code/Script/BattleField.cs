4e45e4c14417a7087c6db9ff5d4518c280264677
reflake <reflakeykt@gmail.com>
2023-02-15 14:30:32 +0600

    set battlefield collider walls

--- Assets/Code/Script/BattleField.cs
+++ Assets/Code/Script/BattleField.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class BattleField : MonoBehaviour
 {
@@ -6,6 +7,27 @@ public class BattleField : MonoBehaviour
 	[SerializeField] private Bounds bounds;
 
 	public Bounds Bounds => bounds;
+
+	private void Awake()
+	{
+		var padding = 1f;
+		var width = new Vector2(bounds.size.x + padding * 2f, padding);
+		var height = new Vector2(padding, bounds.size.y);
+		
+		// Some magic
+		CreateBox(bounds.center + (padding * .5f + bounds.extents.y) * Vector3.up, width);
+		CreateBox(bounds.center + (padding * .5f + bounds.extents.y) * Vector3.down, width);
+		CreateBox(bounds.center + (padding * .5f + bounds.extents.x) * Vector3.left, height);
+		CreateBox(bounds.center + (padding * .5f + bounds.extents.x) * Vector3.right, height);
+	}
+
+	private void CreateBox(Vector2 offset, Vector2 size)
+	{
+		var box = gameObject.AddComponent<BoxCollider2D>();
+
+		box.offset = offset - (Vector2)transform.position;
+		box.size = size;
+	}
 
 	private void OnDrawGizmosSelected()
 	{
