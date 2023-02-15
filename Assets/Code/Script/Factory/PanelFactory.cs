43568c26558cd99c6afe72ce3e8f4d49334f5a6c
reflake <reflakeykt@gmail.com>
2023-02-15 16:44:35 +0600

    add exception when panel is not found

--- Assets/Code/Script/Factory/PanelFactory.cs
+++ Assets/Code/Script/Factory/PanelFactory.cs
@@ -1,6 +1,8 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 using Zenject;
+using Object = UnityEngine.Object;
 
 public class PanelFactory : IPanelFactory
 {
@@ -9,6 +11,10 @@ public class PanelFactory : IPanelFactory
 	public T Create<T>(string prefabPath, Transform parentTransform) where T : Object
 	{
 		var gameObject = Resources.Load<T>(prefabPath);
+
+		if (gameObject == null)
+
+			throw new Exception($"Panel prefab not found: {prefabPath}");
 
 		return _container.InstantiatePrefabForComponent<T>(gameObject, parentTransform);
 	}
