157fd23f8bb869347c6a1c0343acc47c30b99f57
reflake <reflakeykt@gmail.com>
2023-02-15 22:11:30 +0600

    game menu and option to start a game

---
+++ Assets/Code/Script/Menu/MenuManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using Zenject;
+
+namespace Menu
+{
+	public class MenuManager : MonoBehaviour
+	{
+		[Inject] readonly ZenjectSceneLoader _sceneLoader;
+		
+		[SerializeField] Button playerOneButton = default;
+		[SerializeField] Button exitButton = default;
+		
+		void Awake()
+		{
+			playerOneButton.onClick.AddListener(StartGame);
+			exitButton.onClick.AddListener(Exit);
+		}
+
+		void StartGame()
+		{
+			LoadGameplay();
+		}
+
+		async UniTaskVoid LoadGameplay()
+		{
+			await _sceneLoader.LoadSceneAsync("Gameplay", LoadSceneMode.Single);
+		}
+
+		void Exit()
+		{
+#if UNITY_EDITOR
+			UnityEditor.EditorApplication.isPlaying = false;
+#else
+				Application.Quit();
+#endif
+		}
+	}
+}
---
+++ Assets/Code/Script/Menu/StandaloneOnly.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+namespace Menu
+{
+	public class StandaloneOnly : MonoBehaviour
+	{
+		void Awake()
+		{
+			#if !UNITY_STANDALONE
+			
+			gameObject.SetActive(false);
+			
+			#endif 
+		}
+	}
+}
