793c3c9890cfe39cf7e923568cc4522fc93b3ce8
reflake <reflakeykt@gmail.com>
2023-02-21 13:05:12 +0600

    list all levels on device

---
+++ Assets/Code/Script/LevelDesigner/LevelList.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using UnityEngine;
+
+namespace LevelDesigner
+{
+	public class LevelList : MonoBehaviour
+	{
+		void Start()
+		{
+#if UNITY_EDITOR
+			string levelsDirectory = $"{Application.dataPath}/Resources/Level/";
+#else
+			string levelsDirectory = $"{Application.persistentDataPath}/Level/";
+#endif
+			
+			string[] levels = Directory.GetFiles(levelsDirectory);
+			
+			Debug.Log(string.Join("\n",levels));
+		}
+	}
+}
