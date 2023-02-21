ceb0d81593860aef706669246b0502a1a77e81fc
reflake <reflakeykt@gmail.com>
2023-02-21 16:50:30 +0600

    comment tank stats

--- Assets/Code/Script/Tanks/Stats.cs
+++ Assets/Code/Script/Tanks/Stats.cs
@@ -5,11 +5,17 @@ namespace Tanks
 	[Serializable]
 	public struct Stats
 	{
+		// Amount of hits tank can take before destruction
 		public int hitPoints;
+		// How much bullets can shoot tank
 		public int fireRate;
+		// How much tank's bullets penetrate through walls
 		public int firePower;
+		// Bullet damage bonus
 		public int damageBonus;
+		// Movement speed of tank
 		public float moveSpeed;
+		// Bullet speed
 		public float projectileSpeed;
 	}
 }
